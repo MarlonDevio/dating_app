1e01b56fb5acc553bf61afba170b79aab2ef4a11
Marlon Hamaekers <marlon@marlon-m3.home>
2024-02-19 22:29:23 +0100

    small responsive tweaks

--- src/components/styledComponents/Headings/HeadingOne.tsx
+++ src/components/styledComponents/Headings/HeadingOne.tsx
@@ -1,8 +1,12 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const HeadingOne = styled.h1`
   font-size: 9rem;
   color: #fff;
+
+  @media (max-width: 768px) {
+    font-size: 6rem;
+  }
 `;
 
 export default HeadingOne;
